64108a8ba37c8da78884e5ce030d00f8d92830e3
Jens Hansen <jens.hansen@mni.thm.de>
2021-04-20 17:41:10 +0200

    Add skeleton project files using react and semantic ui react

---
+++ src/Arch/Router.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+/**
+ *
+ */
+export class Router extends React.Component {
+
+    /**
+     * @inheritDoc
+     */
+    public render() {
+        return (
+            <div>
+                Router
+            </div>
+        );
+    }
+}
---
+++ src/Pages/Tasks/List.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+/**
+ *
+ */
+export class List extends React.Component {
+
+    /**
+     * @inheritDoc
+     */
+    public render() {
+        return (
+            <div>
+                Task List
+            </div>
+        );
+    }
+}
