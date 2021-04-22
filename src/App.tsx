0f51b306ff205dea2433b9b335eee665bd515ffc
Jens Hansen <jens.hansen@mni.thm.de>
2021-04-22 12:15:41 +0200

    Add Router functionality and Task Form Example file

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
 import {Menu, Container, Header} from "semantic-ui-react";
-import {Router} from "./Arch/Router";
+import {Router as CoreRouter} from "./Arch/Router";
+import {Form as TaskForm} from "./Pages/Tasks/Form";
+import {List as TaskList} from "./Pages/Tasks/List";
+import React from "react";
+import {Router} from "./Components/Router";
 
-function App() {
-  return (
-      <div>
-          <Menu fixed='top' inverted>
-              <Container>
-                  <Menu.Item as='a' header>
-                      Task Manager
-                  </Menu.Item>
-                  <Menu.Item as='a'>Tasks</Menu.Item>
-              </Container>
-          </Menu>
+interface AppProps {
 
-          <Container style={{ marginTop: '5em' }}>
-              <Header as='h1'>Tasks</Header>
-              <Router />
-          </Container>
-      </div>
-  );
+}
+
+interface AppState {
+    page: string;
+}
+
+class App extends React.Component<AppProps, AppState> {
+
+    /**
+     * Switches the app page.
+     */
+    private switchPage(pageName: string): void {
+        debugger;
+
+        CoreRouter.getInstance().switchPage(pageName);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public render() {
+        return (
+            <div>
+                <Menu fixed='top' inverted>
+                    <Container>
+                        <Menu.Item as='a' header>
+                            Task Manager
+                        </Menu.Item>
+                        <Menu.Item as='a' onClick={() => this.switchPage("task.list")}>Tasks</Menu.Item>
+                        <Menu.Item as='a' onClick={() => this.switchPage("task.form")}>Test Task Form</Menu.Item>
+                    </Container>
+                </Menu>
+
+                <Container style={{ marginTop: '5em' }}>
+                    <Router
+                        defaultPage="task.list"
+                        pages={{
+                            "task.list": () => <TaskList/>,
+                            "task.form": () => <TaskForm/>
+                        }}
+                    />
+                </Container>
+            </div>
+        );
+    }
 }
 
 export default App;
